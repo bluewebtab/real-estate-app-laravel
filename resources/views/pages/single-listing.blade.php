a71fd3fe3331d51acadeac55419e85def8728d4b
Ronny <ronny.icne@gmail.com>
2022-09-19 18:42:05 -0700

    Updated tour module in single listing page

--- resources/views/pages/single-listing.blade.php
+++ resources/views/pages/single-listing.blade.php
@@ -10,10 +10,29 @@
         <div class="listing-top__form-wrapper">
             <div class="container">
                 <form class="listing-top__form">
-                    <span class="listing-top__form-title">Schedule A Tour</span>
+                    <label class="listing-top__form-label">Schedule A Tour</label>
                     <div class="listing-top__form-group listing-top__form-group--hor">
                         <div class="listing-top__form-option">In-Person</div>
                         <div class="listing-top__form-option">Video</div>
+                    </div>
+
+                    <label class="listing-top__form-label">Date</label>
+                    <div class="listing-top__form-group listing-top__form-group--hor">
+                        <div class="listing-top__form-option">November 1, 2022</div>
+                    </div>
+                    <label class="listing-top__form-label">Time</label>
+                    <div class="listing-top__form-group">
+                        <div class="listing-top__form-option">11 AM</div>
+                    </div>
+                    <label class="listing-top__form-label">Personal Info</label>
+                    <div class="listing-top__form-group">
+                        <div class="listing-top__form-option">Enter Phone</div>
+                    </div>
+                    <div class="listing-top__form-group">
+                        <div class="listing-top__form-option">Enter Email</div>
+                    </div>
+                    <div class="listing-top__form-group">
+                        <button type="submit" class="listing-top__form-button">Schedule A Tour</button>
                     </div>
                    
                 </form>
